9e49612999101a1ebbdc031dc0a6de3f5c8038cc
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2024-12-28 10:14:58 +0530

    push in existing repo

---
+++ JS/01_hello/hello.js
@@ -0,0 +1,7 @@
+//printing methods in JS
+console.log("hello world");
+process.stdout.write("hello World");
+process.stdout.write("hello World");
+console.table({ name: "admin" });
+console.table({ fruits: "pineapple" });
+console.warn({ name: "admin" });
